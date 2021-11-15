6383328d7432b36df69e8245f0357f94c8bbd28b
Rohit Ramakrishnan <rohitramki@gmail.com>
2021-11-15 18:21:16 -0500

    edited how to access credentials and other fixes

--- emailer.py
+++ emailer.py
@@ -1,46 +1,48 @@
 import os
 import email, smtplib, ssl
-
 from email import encoders
 from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-
 def send(file, recipient):
 	'''Sends an email with file attached to recipient'''
-	# TODO: implement this
-	subject = "PDF Manager File Conversion"
-	body = "Thank you for using the PDF Manager software! The file you had converted is converted is attached to this " \
-		   "email. Please do not reply to this email as it's not monitored. If you have any questions or concerns " \
-		   "please contact us at pdf.manager.help@gmail.com"
-	EMAIL_ADDRESS = os.environ.get('EMAIL_ADDRESS')
-	EMAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+	try:
+		subject = "PDF Manager File Conversion"
+		body = "Thank you for using the PDF Manager software! The file you had converted is converted is attached to this " \
+			   "email. Please do not reply to this email as it's not monitored. If you have any questions or concerns " \
+			   "please contact us at pdf.manager.help@gmail.com"
+		email_credential_container = []
+		with open('email_credentials.txt') as f:
+			email_credential_container = f.readlines()
 
-	message = MIMEMultipart()
-	message["From"] = EMAIL_ADDRESS
-	message["To"] = recipient
-	message["Subject"] = subject
-	message.attach(MIMEText(body, "plain"))
+		EMAIL_ADDRESS = email_credential_container[0]
+		EMAIL_PASSWORD = email_credential_container[1]
 
-	with open(file, "rb") as attachment:
-		part = MIMEBase("application", "octet-stream")
-		part.set_payload(attachment.read())
+		message = MIMEMultipart()
+		message["From"] = EMAIL_ADDRESS
+		message["To"] = recipient
+		message["Subject"] = subject
+		message.attach(MIMEText(body, "plain"))
 
-	encoders.encode_base64(part)
+		with open(file, "rb") as attachment:
+			part = MIMEBase("application", "octet-stream")
+			part.set_payload(attachment.read())
 
-	part.add_header(
-		"Content-Disposition",
-		f"attachment; filename= {file}",
-	)
+		encoders.encode_base64(part)
 
-	message.attach(part)
-	text = message.as_string()
+		part.add_header(
+			"Content-Disposition",
+			f"attachment; filename= {file}",
+		)
 
-	context = ssl.create_default_context()
-	with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
-		server.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
-		server.sendmail(EMAIL_ADDRESS, recipient, text)
+		message.attach(part)
+		text = message.as_string()
 
-#When testing this function enter your email to the second parameter
-#send("test-files/test-one.txt", "")
+		context = ssl.create_default_context()
+		with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
+			server.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+			server.sendmail(EMAIL_ADDRESS, recipient, text)
+		return True
+	except:
+		return False
